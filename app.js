a9f6e0fe7258e6154043601cc0dabad688bb1bb6
kotakunp <kotakunp@gmail.com>
2025-04-15 22:34:00 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const addButton = document.getElementById("addButton");
+const taskInput = document.getElementById("addTask");
+const taskList = document.getElementById("taskList");
+
+addButton.addEventListener("click", addTask);
+
+function addTask() {
+  const taskText = taskInput.value.trim();
+  if (taskText === "") return;
+
+  const li = document.createElement("li");
+  li.className = "task";
+  li.innerHTML = `
+    <span>${taskText}</span>
+    <button onclick="removeTask(this)">Done</button>`;
+
+  taskList.appendChild(li);
+  taskInput.value = "";
+  taskInput.focus();
+}
+
+function removeTask(button) {
+  const taskItem = button.parentElement;
+  taskList.removeChild(taskItem);
+}
